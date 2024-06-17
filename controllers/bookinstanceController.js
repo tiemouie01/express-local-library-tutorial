f0963f1f2e2a93ec47cdb3e456f7a7480d137e5e
Timothy Miamba <timal0361@gmail.com>
2024-06-17 13:19:44 +0200

    feat: Update bookinstance delete form to fetch and display bookinstance details
    This commit modifies the `bookinstanceController.js` file to update the `bookinstance_delete_get` and `bookinstance_delete_post` functions. The `bookinstance_delete_get` function now fetches the details of the bookinstance from the database and renders the `bookinstance_delete` view with the fetched data. The `bookinstance_delete_post` function also fetches the bookinstance and handles the deletion of the bookinstance. After deletion, the user is redirected to the bookinstance list page.
    
    Note: This commit builds upon the previous commits that updated the bookinstance detail page and bookinstance list view.

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -95,12 +95,27 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  // Get details for bookinstance.
+  const bookinstance = await BookInstance.findById(req.params.id);
+
+  if (bookinstance === null) {
+    // No results.
+    res.redirect("catalog/bookinstances");
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete Book Instance",
+    bookinstance: bookinstance,
+  })
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  // Get details for bookinstance.
+  const bookinstance = await BookInstance.findById(req.params.id);
+
+  await BookInstance.findByIdAndDelete(req.params.id);
+  res.redirect("/catalog/bookinstances")
 });
 
 // Display BookInstance update form on GET.

288a454cc1fdd5545658010da72387b49eeefe07
Timothy Miamba <timal0361@gmail.com>
2024-06-17 15:57:53 +0200

    feat: Update genre update form to fetch and display genre details
    This commit modifies the `genreController.js` file to update the `genre_update_get` function. The function now fetches the details of the genre from the database and renders the `genre_form` view with the fetched data. The view includes the genre's current details and allows the user to update the genre's name. The commit also updates the `genre_update_post` function to handle the form submission and update the genre record in the database.

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -129,10 +129,54 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+  // Get genre.
+  const genre = await Genre.findById(req.params.id).exec();
+
+  if (genre === null) {
+    // No results.
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  console.log(genre);
+
+  res.render("genre_form", {
+    title: "Update Genre",
+    genre: genre,
+    errors: null,
+  });
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+  body("name", "Genre name must contain at least 3 characters.")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const previousGenre = Genre.findById(req.params.id).exec();
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre: previousGenre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updatedGenre = await Genre.findByIdAndUpdate(
+        req.params.id,
+        genre,
+        {}
+      );
+      res.redirect(updatedGenre.url);
+    }
+  }),
+];
